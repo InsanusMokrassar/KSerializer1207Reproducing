14dc5396acc304a873b59cbdad1a1f2e60f8dd99
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-11-16 21:30:50 +0600

    make reprodusable

--- src/main/kotlin/telegram_bot/App.kt
+++ src/main/kotlin/telegram_bot/App.kt
@@ -1,13 +1,7 @@
 package telegram_bot
 
-import dev.inmo.tgbotapi.extensions.api.bot.getMe
-import dev.inmo.tgbotapi.extensions.api.telegramBot
+import dev.inmo.tgbotapi.types.ChatMember.abstracts.AdministratorChatMember
 
-/**
- * This method by default expects one argument in [args] field: telegram bot token
- */
-suspend fun main(args: Array<String>) {
-    val bot = telegramBot(args.first())
-
-    println(bot.getMe())
+fun main() {
+//    AdministratorChatMember.serializer()
 }
