cda678257b20abf36bcdfcd05cabe96950094744
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-11-16 21:31:16 +0600

    add comment

--- src/main/kotlin/telegram_bot/App.kt
+++ src/main/kotlin/telegram_bot/App.kt
@@ -3,5 +3,6 @@ package telegram_bot
 import dev.inmo.tgbotapi.types.ChatMember.abstracts.AdministratorChatMember
 
 fun main() {
+    // Uncomment next line to see issue
 //    AdministratorChatMember.serializer()
 }
