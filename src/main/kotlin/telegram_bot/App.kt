7a418c6c2489b3a6324e2ee45c9df414dcd1ff33
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-11-19 18:42:35 +0600

    actualization

--- src/main/kotlin/telegram_bot/App.kt
+++ src/main/kotlin/telegram_bot/App.kt
@@ -1,8 +1,33 @@
 package telegram_bot
 
-import dev.inmo.tgbotapi.types.ChatMember.abstracts.AdministratorChatMember
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+
+@Serializable(Serializer::class)
+interface Example
+@Serializable
+data class StringExample(val data: String) : Example
+
+object Serializer : KSerializer<Example> {
+    override val descriptor: SerialDescriptor = String.serializer().descriptor
+
+    override fun deserialize(decoder: Decoder): Example {
+        val data = decoder.decodeString()
+        return StringExample(data)
+    }
+
+    override fun serialize(encoder: Encoder, value: Example) {
+        encoder.encodeString((value as StringExample).data)
+    }
+
+}
 
 fun main() {
     // Uncomment next line to see issue
-//    AdministratorChatMember.serializer()
+//    Example.serializer()
 }
